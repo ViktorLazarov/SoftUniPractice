9401571c25d1d14c407d5e19256967a425c2157e
viktorlazarov <viktorlazarov@abv.bg>
2017-04-20 20:27:42 +0300

    create HexConverter

--- Numeric_Converter/Numeric_Converter/Program.cs
+++ Numeric_Converter/Numeric_Converter/Program.cs
@@ -10,6 +10,48 @@ namespace Numeric_Converter
     {
         static void Main(string[] args)
         {
+            string inputNumber = Console.ReadLine();
+            string input = Console.ReadLine();
+            string output = Console.ReadLine();
+
+            if (input == "DEC")
+            {
+                if (output == "HEX")
+                {
+                    int number = int.Parse(inputNumber);
+
+
+                }
+                else if (output == "BIN")
+                {
+
+                }
+            }
+            else if (input == "HEX")
+            {
+                if (output == "DEC")
+                {
+
+                }
+                else if (output == "BIN")
+                {
+
+                }
+            }
+            else if (input == "BIN")
+            {
+                if (output == "HEX")
+                {
+
+                }
+                else if (output == "DEC")
+                {
+
+                }
+               
+            }
         }
+
+        
     }
 }
