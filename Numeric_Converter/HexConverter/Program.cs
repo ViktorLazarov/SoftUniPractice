e2efccbe78bd983778d330b7ac963c95b383a123
viktorlazarov <viktorlazarov@abv.bg>
2017-04-20 20:57:29 +0300

    finished DEC

--- Numeric_Converter/HexConverter/Program.cs
+++ Numeric_Converter/HexConverter/Program.cs
@@ -10,6 +10,37 @@ namespace HexConverter
     {
         static void Main(string[] args)
         {
+            string numberInput = Console.ReadLine();
+            string inputSystem = Console.ReadLine();
+            string result = string.Empty;
+            if (inputSystem == "DEC")
+            {
+                string remainder ;
+                int number = int.Parse(numberInput);
+                while (number != 0)
+                {
+                    remainder = (number % 16).ToString();
+                    switch (remainder)
+                    {
+                        case "10":remainder = "A"; break;
+                        case "11": remainder = "B"; break;
+                        case "12": remainder = "C"; break;
+                        case "13": remainder = "D"; break;
+                        case "14": remainder = "E"; break;
+                        case "15": remainder = "F"; break;
+                        
+                    }
+                    number /= 16;
+                    result += remainder;
+                }
+            }
+            else if (inputSystem == "BIN")
+            {
+
+
+
+            }
+            Console.WriteLine(result);
 
         }
     }
