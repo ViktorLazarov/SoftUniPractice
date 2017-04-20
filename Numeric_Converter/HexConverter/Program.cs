032a762828e023882da645941895cdc657710efc
viktorlazarov <viktorlazarov@abv.bg>
2017-04-20 22:08:40 +0300

    finished HEX converrter

--- Numeric_Converter/HexConverter/Program.cs
+++ Numeric_Converter/HexConverter/Program.cs
@@ -10,37 +10,81 @@ namespace HexConverter
     {
         static void Main(string[] args)
         {
+            //Write a program that converts a number(decimal or binary) into hexadecimal format
             string numberInput = Console.ReadLine();
             string inputSystem = Console.ReadLine();
             string result = string.Empty;
             if (inputSystem == "DEC")
             {
-                string remainder ;
+                string remainder;
+
                 int number = int.Parse(numberInput);
                 while (number != 0)
                 {
                     remainder = (number % 16).ToString();
                     switch (remainder)
                     {
-                        case "10":remainder = "A"; break;
+                        case "10": remainder = "A"; break;
                         case "11": remainder = "B"; break;
                         case "12": remainder = "C"; break;
                         case "13": remainder = "D"; break;
                         case "14": remainder = "E"; break;
                         case "15": remainder = "F"; break;
-                        
+
                     }
                     number /= 16;
                     result += remainder;
                 }
+                char[] resultChar = result.ToCharArray();
+                for (int i = resultChar.Length - 1; i >= 0; i--)
+                {
+                    Console.Write(resultChar[i]);
+                }
+                Console.WriteLine();
+                
             }
             else if (inputSystem == "BIN")
             {
+                char[] binInput = numberInput.ToCharArray();
+                double resultNumber = 0;
+                int pow = binInput.Length - 1;
+                for (int i = 0; i <= binInput.Length - 1; i++)
+                {
+                    resultNumber += int.Parse(binInput[i].ToString()) * (Math.Pow(2, pow));
+                    pow--;
+
+                }
+            
+                string remainder;
+                
+                while (resultNumber != 0)
+                {
+                    remainder = (resultNumber % 16).ToString();
+                    switch (remainder)
+                    {
+                        case "10": remainder = "A"; break;
+                        case "11": remainder = "B"; break;
+                        case "12": remainder = "C"; break;
+                        case "13": remainder = "D"; break;
+                        case "14": remainder = "E"; break;
+                        case "15": remainder = "F"; break;
+
+                    }
+                    resultNumber = Math.Floor(resultNumber / 16);
+                    result += remainder;
 
 
+                }
+                char[] resultChar = result.ToCharArray();
+                for (int i = resultChar.Length-1; i >= 0; i--)
+                {
+                    Console.Write(resultChar[i]);
+                }
+                Console.WriteLine();
+                
 
             }
-            Console.WriteLine(result);
+
 
         }
     }
