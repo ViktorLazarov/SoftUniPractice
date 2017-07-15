d4aaa09bce40daaa2794262536d578e6900ceb85
viktorlazarov <viktorlazarov@abv.bg>
2017-07-15 22:01:31 +0300

    finished Beverage Labels

---
+++ TechModuleCSharpIntroAndSintax/4. Beverage Labels/4. Beverage Labels.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Beverage_Labels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            double volume = int.Parse(Console.ReadLine());
+            double energy = int.Parse(Console.ReadLine());
+            double sugar = int.Parse(Console.ReadLine());
+            double multiplier = volume / 100;
+            double totalEnergy = energy * multiplier;
+            double totalSugar = sugar * multiplier;
+
+            Console.WriteLine($"{volume}ml {name}:");
+            Console.WriteLine($"{totalEnergy}kcal, {totalSugar}g sugars");
+
+        }
+    }
+}
