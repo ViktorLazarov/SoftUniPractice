d6f4deb82bf0264644f5cf2adbc463ac814caa04
viktorlazarov <viktorlazarov@abv.bg>
2017-08-17 18:44:45 +0300

    done with task 3

--- TechModuleConditionalStatementsAndLoops/3.Restaurant Discount/Restaurant Discount.cs
+++ TechModuleConditionalStatementsAndLoops/3.Restaurant Discount/Restaurant Discount.cs
@@ -11,67 +11,53 @@ namespace _3.Restaurant_Discount
         static void Main(string[] args)
         {
             int count = int.Parse(Console.ReadLine());
-            string package = Console.ReadLine();
+            string package = Console.ReadLine().ToLower();
             int packagePrice = 0;
             double discount = 0;
             int hallPrice = 0;
             int totalPrice = 0;
+            string hallName = "";
 
             switch (package)
             {
-                case "Normal": discount = 0.95; packagePrice = 500; break;
-                case "Gold": discount = 0.90; packagePrice = 750; break;
-                case "Platinum": discount = 0.85; packagePrice = 1000; break;
+                case "normal": discount = 0.95; packagePrice = 500; break;
+                case "gold": discount = 0.90; packagePrice = 750; break;
+                case "platinum": discount = 0.85; packagePrice = 1000; break;
 
             }
-            if (count > 0 && count <= 50)
+            if (count > 0 && count <= 120)
             {
-                hallPrice = 2500;
-                string hallName = "Small Hall";
+                if (count <= 50)
+                {
+                    hallPrice = 2500;
+                    hallName = "Small Hall";
+
+
+                }
+                else if (count > 50 && count <= 100)
+                {
+                    hallPrice = 5000;
+                    hallName = "Terrace";
+
+                }
+                else if (count > 100 && count <= 120)
+                {
+                    hallPrice = 7500;
+                    hallName = "Grand Hall";
+
+                }
                 totalPrice = hallPrice + packagePrice;
                 double finalPrice = totalPrice * discount;
                 double pricePerPerson = finalPrice / count;
 
                 Console.WriteLine("We can offer you the {0}", hallName);
                 Console.WriteLine("The price per person is {0:F2}$", pricePerPerson);
-
-            }
-            else if (count > 50 && count <= 100)
-            {
-                hallPrice = 5000;
-                string hallName = "Terrace";
-                totalPrice = hallPrice + packagePrice;
-                double finalPrice = totalPrice * discount;
-                double pricePerPerson = finalPrice / count;
-
-                Console.WriteLine("We can offer you the {0}", hallName);
-                Console.WriteLine("The price per person is {0:F2}$", pricePerPerson);
-            }
-            else if (count > 100 && count <= 120)
-            {
-                hallPrice = 7500;
-                string hallName = "Grand Hall";
-                totalPrice = hallPrice + packagePrice;
-                double finalPrice = totalPrice * discount;
-                double pricePerPerson = finalPrice / count;
-
-                Console.WriteLine("We can offer you the {0}", hallName);
-                Console.WriteLine("The price per person is {0:F2}$", pricePerPerson);
-                
-            }
-            else if(count > 120)
-            {
-                Console.WriteLine("We do not have an appropriate hall.");
             }
             else
             {
-                Console.WriteLine("Error");
+                Console.WriteLine("We do not have an appropriate hall.");
             }
-            
-
-            
-
-
+           
         }
     }
 }
