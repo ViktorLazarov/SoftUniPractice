9c3b3fdd15f147d8935e871374ac4348f917a31e
viktorlazarov <viktorlazarov@abv.bg>
2017-08-19 23:20:42 +0300

    finished task 6

---
+++ TechModuleConditionalStatementsAndLoops/6.Interval of Numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.Interval_of_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            if (firstNumber>secondNumber)
+            {
+                for (int i = secondNumber; i <= firstNumber; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            else
+            {
+                for (int i = firstNumber; i <= secondNumber; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
