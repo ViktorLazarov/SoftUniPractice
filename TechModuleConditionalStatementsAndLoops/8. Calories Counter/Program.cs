9deb1aaef5f948c51459b06741723e468b7c7f64
viktorlazarov <viktorlazarov@abv.bg>
2017-08-19 23:45:50 +0300

    finish task 8

---
+++ TechModuleConditionalStatementsAndLoops/8. Calories Counter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8.Calories_Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int cheeseCalories = 500;
+            int tomatoSauceCalories = 150;
+            int salamiCalories = 600;
+            int pepperCalories = 50;
+            int totalCalories = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                string ingredient = Console.ReadLine().ToLower();
+                switch (ingredient)
+                {
+                    case "cheese":totalCalories += cheeseCalories; break;
+                    case "tomato sauce":totalCalories += tomatoSauceCalories; break;
+                    case "salami":totalCalories += salamiCalories; break;
+                    case "pepper":totalCalories += pepperCalories; break;
+                }
+            }
+            Console.WriteLine($"Total calories: {totalCalories}");
+        }
+    }
+}
