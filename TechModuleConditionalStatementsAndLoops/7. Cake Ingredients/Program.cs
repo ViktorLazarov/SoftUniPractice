c7fae74b8d2a63c1d7a5396ccdef3fa45fa7abe8
viktorlazarov <viktorlazarov@abv.bg>
2017-08-19 23:32:24 +0300

    finished task 7

---
+++ TechModuleConditionalStatementsAndLoops/7. Cake Ingredients/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.Cake_Ingredients
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+            int counter = 0;
+            while (true)
+            {
+                input = Console.ReadLine();
+                if (input == "Bake!")
+                {
+                    Console.WriteLine($"Preparing cake with {counter} ingredients.");
+                    break;
+                }
+                Console.WriteLine($"Adding ingredient {input}.");
+                counter++;
+            }
+            
+        }
+    }
+}
