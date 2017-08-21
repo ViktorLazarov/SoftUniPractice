7b6339b2eee601b0d34aa3d551a0ccbdb4f0da5e
viktorlazarov <viktorlazarov@abv.bg>
2017-08-21 20:13:37 +0300

    finished task 10

---
+++ TechModuleConditionalStatementsAndLoops/10.Triangle of Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Triangle_of_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int row = 1; row <= n; row++)
+            {
+                for (int j = 0; j < row; j++)
+                {
+                    Console.Write(row + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
