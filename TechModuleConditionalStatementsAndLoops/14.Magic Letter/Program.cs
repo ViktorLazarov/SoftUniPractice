430961239b79053e672716a847f5a04058f8099a
viktorlazarov <viktorlazarov@abv.bg>
2017-08-21 22:30:50 +0300

    finished task 14

---
+++ TechModuleConditionalStatementsAndLoops/14.Magic Letter/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.Magic_Letter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstLetter = char.Parse(Console.ReadLine());
+            char secondLetter = char.Parse(Console.ReadLine());
+            char magicLetter = char.Parse(Console.ReadLine());
+
+            for (char i = firstLetter; i <= secondLetter; i++)
+            {
+                for (char j = firstLetter; j <= secondLetter; j++)
+                {
+                    for (char k = firstLetter; k <= secondLetter; k++)
+                    {
+                        if (i != magicLetter && j != magicLetter && k != magicLetter)
+                        {
+                            Console.Write($"{i}{j}{k} ");
+                        }
+                        
+                    }
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
