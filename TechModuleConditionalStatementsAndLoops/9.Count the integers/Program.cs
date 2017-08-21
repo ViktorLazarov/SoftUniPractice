d858485ee49d88afc8fd93ad25e2f97c52ca59e7
viktorlazarov <viktorlazarov@abv.bg>
2017-08-21 19:50:28 +0300

    finished task 9

---
+++ TechModuleConditionalStatementsAndLoops/9.Count the integers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9.Count_the_integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int counter = 0;
+            int number;
+            while (true)
+            {
+                bool result = Int32.TryParse(input, out number);
+
+                if (result)
+                {
+                    counter++;
+                }
+                else
+                {
+                    Console.WriteLine(counter);
+                    break;
+                }
+                input = Console.ReadLine();
+            }
+        
+        }
+    }
+}
