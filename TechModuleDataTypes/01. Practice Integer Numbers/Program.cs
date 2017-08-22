ede3902f1f93c228631889ce963a65813e466632
viktorlazarov <viktorlazarov@abv.bg>
2017-08-22 21:26:34 +0300

    .

---
+++ TechModuleDataTypes/01. Practice Integer Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Practice_Integer_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sdfdsf");
+        }
+    }
+}
