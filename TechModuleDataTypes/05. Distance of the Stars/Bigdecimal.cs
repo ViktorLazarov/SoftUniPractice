17764a1c44c6f05d7ec3343da1cf90bafed12607
viktorlazarov <viktorlazarov@abv.bg>
2017-08-28 23:02:27 +0300

    finish task 5

---
+++ TechModuleDataTypes/05. Distance of the Stars/Bigdecimal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _05.Distance_of_the_Stars
+{
+    internal class Bigdecimal
+    {
+        public static implicit operator Bigdecimal(long v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
